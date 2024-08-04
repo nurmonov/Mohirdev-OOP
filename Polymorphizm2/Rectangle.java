1ce0cc0b326b58c54e1fc870916a1abceace8a03
nurmonov <nurmonoff@gmail.com>
2024-08-04 20:07:58 +0500

    Add files via upload
    2 masala

---
+++ Polymorphizm2/Rectangle.java
@@ -0,0 +1,45 @@
+package Polymorphizm2;
+
+public class Rectangle  extends GeometricFigure{
+    private int a;
+    private int b;
+
+    public Rectangle(int sideA, int sideB) {
+        this.a = sideA;
+        this.b = sideB;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    @Override
+    public double rectanglyArea(){
+        return a*b;
+    }
+
+    @Override
+    public double rectanglyPeremetr() {
+        return (a+b)*2;
+    }
+
+}
+class Test{
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(6, 5);
+        System.out.println("rectangle.rectanglyArea() = " + rectangle.rectanglyArea());
+        System.out.println("rectangle.rectanglyPeremetr() = " + rectangle.rectanglyPeremetr());
+    }
+}
